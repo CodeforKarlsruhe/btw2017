f2e2b5ecfc3863d29b129bb3e33d56faa19b0710
Florian Brucker <florian.brucker@it.karlsruhe.de>
2017-09-11 15:58:38 +0200

    Parsen von Zweitstimmen, CSV- und JSON-Export

--- wahldaten-ka/scraper/ka_wahldaten_scraper.py
+++ wahldaten-ka/scraper/ka_wahldaten_scraper.py
@@ -1,8 +1,57 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+'''
+Scraper for election results for the Bundestag election for Karlsruhe.
+
+The results are published online as an HTML-export from PC-Wahl. This
+scraper uses the per-district results from that export for both the
+first and second votes and combines them into a single dataset.
+'''
+
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
+
+import re
+
+
+BOROUGHS = {
+    1: 'Innenstadt-Ost',
+    2: 'Innenstadt-West',
+    3: 'Südstadt',
+    4: 'Südweststadt',
+    5: 'Weststadt',
+    6: 'Nordweststadt',
+    7: 'Oststadt',
+    8: 'Mühlburg',
+    9: 'Daxlanden',
+    10: 'Knielingen',
+    11: 'Grünwinkel',
+    12: 'Oberreut',
+    13: 'Beiertheim-Bulach',
+    14: 'Weiherfeld-Dammerstock',
+    15: 'Rüppurr',
+    16: 'Waldstadt',
+    17: 'Rintheim',
+    18: 'Hagsfeld',
+    19: 'Durlach',
+    20: 'Grötzingen',
+    21: 'Stupferich',
+    22: 'Hohenwettersbach',
+    23: 'Wolfartsweier',
+    24: 'Grünwettersbach',
+    25: 'Palmbach',
+    26: 'Neureut',
+    27: 'Nordstadt',
+}
+
+
+def district_to_borough(district_num):
+    '''
+    Return the borough number for a district.
+    '''
+    parts = district_num.split('-')
+    return int(parts[0])
 
 
 def extract_table(table):
@@ -41,52 +90,132 @@ def parse_german_number(s):
         return None
 
 
+def collapse_whitespace(s):
+    '''
+    Collapse all adjacent whitespace to a single space and strip the string.
+    '''
+    return re.sub(r'\s+', ' ', s).strip()
+
+
 if __name__ == '__main__':
 
-    try:
-        # Python 2
-        from urllib import urlopen
-    except ImportError:
-        # Python 3
+    import sys
+    if sys.version_info.major == 3:
         from urllib.request import urlopen
+        import csv
+    else:
+        from urllib import urlopen
+        from backports import csv
+    import io
+    import json
 
     from bs4 import BeautifulSoup
 
-    URL = 'http://web3.karlsruhe.de/Stadtentwicklung/afsta/Wahlen/Wahlabend-Netmodul/2013-btw/erst/bundestag-2013-erst-wbz.php'
+    # fv = first vote, sv = second vote
 
-    html = urlopen(URL).read()
-    soup = BeautifulSoup(html, 'html.parser')
-    tables = soup.find_all('table')
-    assert len(tables) == 2
-    data = extract_table(tables[1])
+    FV_URL = 'http://web3.karlsruhe.de/Stadtentwicklung/afsta/Wahlen/Wahlabend-Netmodul/2013-btw/erst/bundestag-2013-erst-wbz.php'
+    SV_URL = 'http://web3.karlsruhe.de/Stadtentwicklung/afsta/Wahlen/Wahlabend-Netmodul/2013-btw/zweit/bundestag-2013-zwei-wbz.php'
 
-    header = data[0]
-    direct_candidates = header[5:]
+    def get_data_from_url(url):
+        '''
+        Get tabular vote data from an URL.
+        '''
+        html = urlopen(url).read()
+        soup = BeautifulSoup(html, 'html.parser')
+        tables = soup.find_all('table')
+        assert len(tables) == 2
+        return extract_table(tables[1])
+
+    fv_data = get_data_from_url(FV_URL)
+    sv_data = get_data_from_url(SV_URL)
+    assert len(fv_data) == len(sv_data)
+
+    fv_header = fv_data[0]
+    sv_header = sv_data[0]
+    candidates = [collapse_whitespace(s) for s in fv_header[5:]]
+    parties = [collapse_whitespace(s) for s in sv_header[5:]]
+
+    def parse_votes(row, names):
+        '''
+        Parse the first/second votes of a single voting district.
+        '''
+        votes = {}
+        for cell, name in zip(row[5:], names):
+            parts = cell.split()
+            num_votes = parse_german_number(parts[0])
+            votes[name] = num_votes
+        return votes
+
+    # Parse data for each district
     districts = {}
-    for row in data[1:]:
-        district_num = fix_district_number(row[0])
+    for fv_row, sv_row in zip(fv_data[1:], sv_data[1:]):
+        for i in range(5):
+            assert fv_row[i] == sv_row[i]
+        district_num = fix_district_number(fv_row[0])
+        borough_num = district_to_borough(district_num)
         district = {
-            #'Wahlkreisnummer': 271,
-            #'Wahlkreisname': 'Karlsruhe-Stadt',
-            #'Stadtteilnummer': 'FIXME',
-            #'Stadtteilname:': 'FIXME',
+            'Wahlkreisnummer': 271,
+            'Wahlkreisname': 'Karlsruhe-Stadt',
+            'Stadtteilnummer': borough_num,
+            'Stadtteilname': BOROUGHS.get(borough_num),
             'Wahlbezirksnummer': district_num,
-            'Wahlbezirksname': row[1],
-            'Wahlberechtigte insgesamt': parse_german_number(row[2]),
-            'Wähler/-innen': parse_german_number(row[3]),
-            'Wahlbeteiligung': parse_german_number(row[4].rstrip('%')),
+            'Wahlbezirksname': fv_row[1],
+            'Wahlberechtigte insgesamt': parse_german_number(fv_row[2]),
+            'Wähler/-innen': parse_german_number(fv_row[3]),
+            'Wahlbeteiligung': parse_german_number(fv_row[4].rstrip('%')),
         }
         # Extract votes for each candidate
-        valid_votes = 0
-        for cell, candidate in zip(row[5:], direct_candidates):
-            parts = cell.split()
-            votes = parse_german_number(parts[0])
-            district[candidate] = votes
-            valid_votes += votes
-        district['Gültige Erststimmen'] = valid_votes
-
+        district['Erststimmen'] = parse_votes(fv_row, candidates)
+        district['Gültige Erststimmen'] = sum(district['Erststimmen'].values())
+        district['Zweitstimmen'] = parse_votes(sv_row, parties)
+        district['Gültige Zweitstimmen'] = sum(district['Zweitstimmen'].values())
         districts[district_num] = district
 
-    from pprint import pprint
-    pprint(districts)
+    # Combine postal votes into a single row
+    postal_votes = {
+        'Wahlkreisnummer': 271,
+        'Wahlkreisname': 'Karlsruhe-Stadt',
+        'Stadtteilname': 'Briefwahl',
+        'Stadtteilnummer': None,
+        'Wahlbezirksnummer': None,
+        'Wahlbezirksname': None,
+        'Wahlbeteiligung': None,
+        'Wahlberechtigte insgesamt': None,
+        'Wähler/-innen': 0,
+        'Erststimmen': {},
+        'Zweitstimmen': {},
+    }
+    for district_num in districts.keys():
+        if districts[district_num]['Wahlbezirksname'] == 'Briefwahl':
+            district = districts.pop(district_num)
+            postal_votes['Wähler/-innen'] += district['Wähler/-innen']
+            for vote_type in 'Erststimmen', 'Zweitstimmen':
+                for key, value in district[vote_type].iteritems():
+                    postal_votes[vote_type][key] = postal_votes[vote_type].get(key, 0) + value
+                postal_votes['Gültige ' + vote_type] = sum(postal_votes[vote_type].values())
+    districts['Briefwahl'] = postal_votes
+
+    # Export to CSV
+    CSV_COLUMNS = (['Wahlkreisnummer', 'Wahlkreisname', 'Stadtteilnummer',
+                   'Stadtteilname', 'Wahlbezirksnummer', 'Wahlbezirksname',
+                   'Wahlberechtigte insgesamt', 'Wähler/-innen',
+                   'Gültige Erststimmen'] + candidates + ['Gültige Zweitstimmen']
+                   + parties)
+    with io.open('results.csv', 'w', newline='', encoding='utf-8') as f:
+        writer = csv.writer(f)
+        writer.writerow(CSV_COLUMNS)
+        for district_num in sorted(districts):
+            d = districts[district_num]
+            row = [d['Wahlkreisnummer'], d['Wahlkreisname'], d['Stadtteilnummer'],
+                   d['Stadtteilname'], d['Wahlbezirksnummer'], d['Wahlbezirksname'],
+                   d['Wahlberechtigte insgesamt'], d['Wähler/-innen'],
+                   d['Gültige Erststimmen']]
+            row.extend(d['Erststimmen'][c] for c in candidates)
+            row.append(d['Gültige Zweitstimmen'])
+            row.extend(d['Zweitstimmen'][p] for p in parties)
+            writer.writerow(row)
+
+    # Export to JSON
+    with open('results.json', 'w') as f:
+        json.dump(districts, f)
 
